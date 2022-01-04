5a6df9aff114d09858df1b3f7b880f79a1d686b1
sankeerthankam <sankeerth.ankam@gmail.com>
2022-01-04 14:52:38 -0600

    Create 01. Valid Anagram.py
        # Approach 1 - Use Counter from Collections
        # Approach 2 - Implement Counter and Compare the Counter objects for the two strings

---
+++ Code/Py Files/01. Valid Anagram.py
@@ -0,0 +1,21 @@
+def isAnagram(s, t):
+    # Approach 1 - Use Counter from Collections
+    # Approach 2 - Implement Counter and Compare the Counter objects for the two strings        
+    def Counter(st):
+        d = {}
+        for i in st:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        return d
+
+    if len(s) == len(t):
+        s_ = Counter(s)
+        t_ = Counter(t)
+        if s_ == t_:
+            return True
+        else:
+            return False
+    else:
+        False
