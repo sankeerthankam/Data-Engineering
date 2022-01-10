8ce8b229ff5cf812ea2ac2c9b2900f1775271bec
sankeerthankam <sankeerth.ankam@gmail.com>
2022-01-10 14:58:05 -0600

    Update 02. Merge Sorted Array.py

--- Code/Py Files/02. Merge Sorted Array.py
+++ Code/Py Files/02. Merge Sorted Array.py
@@ -1,9 +1,9 @@
 def merge(nums1, m, nums2, n):
     """
     Do not return anything, modify nums1 in-place instead.
-    """    
+    """
+    # Approach 1: Three pointer approach
     p1, p2, p3 = m - 1, n - 1, len(nums1) - 1
-
     while p1 >= 0 and p2 >= 0:
         if nums2[p2] > nums1[p1]:
             nums1[p3] = nums2[p2]
@@ -15,6 +15,7 @@ def merge(nums1, m, nums2, n):
     nums1[:p2 + 1] = nums2[:p2 + 1]
     
     
+    # Approach 2: Using m and n instead of pointers
     last = m + n -1
     while m > 0 and n > 0:
         if nums1[m - 1] > nums2[n - 1]:
@@ -24,8 +25,10 @@ def merge(nums1, m, nums2, n):
             nums1[last] = nums2[n - 1]
             n = n - 1
         last = last - 1
+
     while n > 0:
         nums1[last] = nums2[n-1]
         n = n - 1
         last = last - 1
+
     return nums1
